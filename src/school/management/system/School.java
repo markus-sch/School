2df39e9419d4e436dd4c4a7487638db47d450de3
markus-sch <markusschlussmeier@gmail.com>
2021-04-30 19:02:58 +0200

    Ergänzen von Lehrern und Studenten

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -26,4 +26,69 @@ public class School {
         totalMoneyEarned = 0;
         totalMoneySpent = 0;
     }
+
+    /**
+     *
+     * @return the list of teachers in the school.
+     */
+    public List<school.management.system.Teacher> getTeachers() {
+        return teachers;
+    }
+
+    /**
+     * Adds a teacher to the school
+     * @param teacher the teacher to be added.
+     */
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    /**
+     *
+     * @return the lis of students in the school
+     */
+    public List<school.management.system.Student> getStudents() {
+        return students;
+    }
+
+    /**
+     * Adds a student to the school
+     * @param student the student to be added
+     */
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    /**
+     *
+     * @return the total money earned by the school.
+     */
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    /**
+     * Adds the total money earned by the school.
+     * @param MoneyEarned money that is supposed to be added.
+     */
+    public void updateTotalMoneyEarned(int MoneyEarned) {
+        totalMoneyEarned += MoneyEarned;
+    }
+
+    /**
+     *
+     * @return the total money spent by the school.
+     */
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    /**
+     * update the money that is spent by the school
+     * which is the salary given by the school to its teachers.
+     * @param MoneySpent the money spent by the school.
+     */
+    public void updateTotalMoneySpent(int moneySpent) {
+        totalMoneyEarned -= moneySpent;
+    }
 }
